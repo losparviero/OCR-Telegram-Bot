3e0a0ee3e05cedc0991169837983d86b93f57263
Zubin <me@zubin.org.in>
2023-03-14 10:01:52 +0530

    oc

--- bot.js
+++ bot.js
@@ -1,18 +1,42 @@
+require("dotenv").config();
+const { Bot } = require("grammy");
 const tesseract = require("node-tesseract-ocr");
 
-const config = {
-  lang: "eng",
-  oem: 1,
-  psm: 3,
-};
+const bot = new Bot(process.env.BOT_TOKEN);
 
-const img = "https://tesseract.projectnaptha.com/img/eng_bw.png";
+// Commands
 
-tesseract
-  .recognize(img, config)
-  .then((text) => {
-    console.log("Result:", text);
-  })
-  .catch((error) => {
-    console.log(error.message);
-  });
+bot.command("start", async (ctx) => {
+  await ctx.reply("*Welcome!* ✨\nSend an image to get words from it.");
+});
+
+// Messages
+
+bot.on("message:text", async (ctx) => {
+  await ctx.reply("*Send a valid image.*");
+});
+
+// OCR
+
+bot.on("message:photo", async (ctx) => {
+  const file = await ctx.getFile();
+  const path = file.file_path;
+
+  const config = {
+    lang: "eng",
+    oem: 1,
+    psm: 3,
+  };
+
+  await tesseract
+    .recognize(path, config)
+    .then(async (text) => {
+      console.log("Result:", text);
+      await ctx.reply(text);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+});
+
+bot.start();
